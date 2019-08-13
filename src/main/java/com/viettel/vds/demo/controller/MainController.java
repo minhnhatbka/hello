076bc47862bf30ab2a78b69d41d1c35d01af1718
minhnhatbka <minhnhatbka@gmail.com>
2019-08-13 16:41:17 +0700

    20190813 fix

--- src/main/java/com/viettel/vds/demo/controller/MainController.java
+++ src/main/java/com/viettel/vds/demo/controller/MainController.java
@@ -1,16 +1,27 @@
 package com.viettel.vds.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Files;
+
 @RestController
 public class MainController {
+
+    @Value("classpath:config.yaml")
+    Resource resourceFile;
 
     @GetMapping(value = "/")
     @ResponseBody
     public String get() {
-        return "Hello nhat";
+        try {
+            return new String(Files.readAllBytes(resourceFile.getFile().toPath()));
+        } catch (Exception e) {
+            return "Config not found";
+        }
     }
 
 }
