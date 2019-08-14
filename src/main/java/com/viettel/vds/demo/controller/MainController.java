063549a36abd0ed4de19a5b5e8a477248d4578ea
minhnhatbka <minhnhatbka@gmail.com>
2019-08-14 18:37:00 +0700

    fix

--- src/main/java/com/viettel/vds/demo/controller/MainController.java
+++ src/main/java/com/viettel/vds/demo/controller/MainController.java
@@ -1,26 +1,21 @@
 package com.viettel.vds.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Files;
-
 @RestController
 public class MainController {
 
-    @Value("classpath:config/config.yml")
-    Resource resourceFile;
+    @Value("${env_version}")
+    private String envVersion;
 
     @GetMapping(value = "/")
     @ResponseBody
     public String get() {
         try {
-            System.out.println(resourceFile.toString());
-            System.out.println(resourceFile.getFile());
-            return new String(Files.readAllBytes(resourceFile.getFile().toPath()));
+            return "Hello from " + envVersion;
         } catch (Exception e) {
             e.printStackTrace();
             return "Config not found";
